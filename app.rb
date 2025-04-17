b25a12d9ef03c4f94de2628243f18469107ccee6
nehir-mentes <nehirmen.98@gmail.com>
2025-04-17 18:47:11 +0000

    sqrt

--- app.rb
+++ app.rb
@@ -1,17 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get ("/howdy") do
-  erb(:hello)
+# Homepage
+get("/") do
+  erb(:new_square)
 end
 
+#Square
 get ("/square/new") do
   erb(:new_square)
 end
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get ("/square/results") do
+  @the_num = params.fetch("number").to_f
+  @the_square = @the_num ** 2
+  erb(:square_results)
+end
+
+#Square Root
+get ("/square_root/new") do
+  erb(:new_square_root)
+end
+
+get ("/square_root/results") do
+  @the_num = params.fetch("number").to_f
+  @the_square_root = @the_num ** 0.5
+  erb(:square_root_results)
 end
