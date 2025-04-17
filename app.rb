09244de6fe3801f2a18f460c0bd99c24570c8e63
nehir-mentes <nehirmen.98@gmail.com>
2025-04-17 18:17:47 +0000

    beginning

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get ("/howdy") do
+  erb(:hello)
+end
+
+get ("/square/new") do
+  erb(:new_square)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
